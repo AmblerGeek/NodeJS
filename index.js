15985255442f6dc0f7c84702eb2152d2b8bfa614
Frank Musick <fjmusick@hotmail.com>
2026-05-27 08:57:30 -0400

    Tweaks - 2026-05-27

--- index.js
+++ index.js
@@ -17,41 +17,85 @@ app.use(cors());
 const tours_data = fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`);
 const tours = JSON.parse(tours_data);
 
-app.get('/api/v1/tours', (request, response) => {
-
+const getAllTours = (request, response) => {
     response
     .status(200)
     .json({
         status: 'success',
+        results: tours.length,
         data: {
             tours
         }
     });
-});
+};
 
-app.post('/api/v1/tours', (request, response) => {
-    console.log(request.body);
-    response
-    .status(200)
-    .send("POST");
-});
+const getTour = (request, response) => {
+    const id = Number(request.params.id);
+    const tour = tours.find(element => element.id == id);
 
-
-/*
-app.get('/', (request, response) => {
-    response
-    .status(200)
-    .json({
-        message: "Hello World!",
-        app: "NodeJS"
+    if(tour) {
+        response
+        .status(200)
+        .json({
+            status: 'success',
+            data: {
+                tour
+            }
         });
-});
+    } else {
+        response.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        });
+    }
+};
 
-app.post('/', (request, response) => {
-    response
-    .send("Post end-point");
-});
-*/
+const createTour = (request, response) => {
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({id: newId,}, request.body);
+    tours.push(newTour);
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), error => {
+        response.status(201).json({
+            status: 'success',
+            data: {
+                tour: newTour
+            }
+        });
+    });
+};
+
+const updateTour = (request, response) => {
+    const id = Number(request.params.id);
+    const tour = tours.find(element => element.id == id);
+    if(tour) {
+        response
+        .status(200)
+        .json({
+            status: 'success',
+            data: {
+                tour: 'Updated Tour'
+            }
+        });
+    } else {
+        response.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        });
+    }
+};
+
+// app.get('/api/v1/tours', getAllTours);
+// app.get('/api/v1/tours/:id', getTour);
+// app.post('/api/v1/tours', createTour);
+// app.patch('/api/v1/tours/:id', updateTour);
+
+app.route('/api/v1/tours')
+    .get(getAllTours)
+    .post(createTour);
+
+app.route('/api/v1/tours/:id')
+    .get(getTour)
+    .patch(updateTour);
 
 const port = config.PORT || 5000;
 app.listen(port, () => {
